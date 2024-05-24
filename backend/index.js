343d10db22eed0614e6253c4f4e44d707a332343
Vandana kumari <vandanakumari@Vandanas-MacBook-Air.local>
2024-05-24 12:44:19 +0530

    commit

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ config();
 
 const app = express();
 
-app.use(cors())
+app.use(cors({origin: '*'}))
 app.use(express.json({limit: "5mb", extended: true}))
 app.use(express.urlencoded({limit: "5mb", extended: true}))
 
